d57cdb8d23363aae4b3365db49d2093318eaa468
Sinan URUN <sinanurun24@gmail.com>
2024-02-25 17:46:45 +0300

    ikinci gun sonu
    setting modeli oluşturuldu

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from home.models import Setting
+
 # Register your models here.
+admin.site.register(Setting)
--- home/models.py
+++ home/models.py
@@ -1,1 +1,28 @@
 from django.db import models
+class Setting(models.Model):
+    STATUS = (('True', 'True'), ('False', 'False'),)
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    company = models.CharField(max_length=50)
+    address = models.CharField(blank=True, max_length=100)
+    phone = models.CharField(blank=True, max_length=15)
+    fax = models.CharField(blank=True, max_length=15)
+    email = models.CharField(blank=True, max_length=50)
+    smtpserver = models.CharField(blank=True, max_length=50)
+    smtpemail = models.CharField(blank=True, max_length=50)
+    smtppassword = models.CharField(blank=True, max_length=10)
+    smtpport = models.CharField(blank=True, max_length=5)
+    icon = models.ImageField(blank=True, upload_to='images/')
+    facebook = models.CharField(blank=True, max_length=50)
+    instagram = models.CharField(blank=True, max_length=50)
+    twitter = models.CharField(blank=True, max_length=50)
+    youtube = models.CharField(blank=True, max_length=50)
+    aboutus = models.TextField()  # RichTextUploadingField()
+    contact = models.TextField()
+    references = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
