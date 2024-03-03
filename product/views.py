39ce89c004168f8913d4733d7b629103bcdde3c1
Sinan URUN <sinanurun24@gmail.com>
2024-03-03 14:23:41 +0300

    urun kategori sayfası tamamlandı
    urun kategori sayfası tamamlandı

--- product/views.py
+++ product/views.py
@@ -6,7 +6,7 @@ from product.models import Category, Product
 # Create your views here.
 def categoryProducts(request, id, slug):
     urunKategori = Category.objects.get(id=id)
-    urunler = Product.objects.get(category_id=id)
+    urunler = Product.objects.filter(category_id=id)
 
     context = {"urunKategori":urunKategori,
                "urunler":urunler}
