88645bed53bbd684d6cb58d108d200d209661169
Sinan URUN <sinanurun24@gmail.com>
2024-03-03 12:06:14 +0300

    form tasarımının model forma uygulanması
    form tasarımının model forma uygulanması

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 from home.models import ContactFormMessage
 
@@ -7,3 +7,18 @@ class ContactForm(ModelForm):
     class Meta:
         model = ContactFormMessage
         fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': TextInput(
+                attrs={'type': "text", 'class': "form-control", 'id': "name", 'placeholder': "Your Name",
+                       'required': "required",
+                       'data-validation-required-message ': "Please enter your name"}),
+            'email': TextInput(
+                attrs={'type': 'email', 'class': "form-control", 'id': "email", 'placeholder': "Your Email",
+                       'required': "required", 'data-validation-required-message': "Please enter your email"}),
+            'subject': TextInput(
+                attrs={'type': "text", 'class': "form-control", 'id': "subject", 'placeholder': "Subject",
+                       'required': "required", 'data-validation-required-message': "Please enter a subject"}),
+            'message': Textarea(attrs={'class': "form-control", 'rows': "6", 'id': "message", 'placeholder': "Message",
+                                       'required': "required",
+                                       'data-validation-required-message': "Please enter your message"}),
+        }
