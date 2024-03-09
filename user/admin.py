90249cdd18578bb6f5d5b031290a3ee55cf33d79
Sinan URUN <sinanurun24@gmail.com>
2024-03-09 11:53:58 +0300

    user_profil model oluşturuldu
    user_profil model oluşturuldu

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from user.models import UserProfile
+
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'address', 'phone_number', 'city', 'country', 'image_tag']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.safestring import mark_safe
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    address = models.CharField(max_length=1500, blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+    image = models.ImageField(upload_to='images/users/', default='images/users/default.jpg')
+    def __str__(self):
+        return self.user.username
+    def user_name(self):
+        return self.user.first_name + ' ' + self.user.last_name + '[' + self.user.username + ']'
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
