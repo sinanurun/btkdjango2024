e536a9941c1e1e2f63d9783e8533ee0c396adb97
Sinan URUN <sinanurun24@gmail.com>
2024-03-10 10:20:03 +0300

    order işlemleri ve admin
    order işlemleri ve admin

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
 
-from order.models import AddFavorite
+from order.models import AddFavorite, Order, OrderProduct, ShopCart
+
+
+# Register your models here.
+class ShopCartAdmin(admin.ModelAdmin):
+    list_display = ['product','user','quantity','price','amount' ]
+    list_filter = ['user']
+
+class FavoriteProductsAdmin(admin.ModelAdmin):
+    list_display = ['product','user']
+    list_filter = ['user']
+
+
+class OrderProductline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('user', 'product','price','quantity','amount')
+    can_delete = False
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name','phone','city','total','status']
+    list_filter = ['status']
+    readonly_fields = ('user','address','city','country','phone','first_name','ip', 'last_name','phone','city','total')
+    can_delete = False
+    inlines = [OrderProductline]
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product','price','quantity','amount']
+    list_filter = ['user']
 
 # Register your models here.
 admin.site.register(AddFavorite)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
+admin.site.register(ShopCart)
--- order/models.py
+++ order/models.py
@@ -72,22 +72,3 @@ class OrderProduct(models.Model):
 
     def __str__(self):
         return self.product.title
-
-class OrderProduct(models.Model):
-    STATUS = (
-        ('New', 'New'),
-        ('Accepted', 'Accepted'),
-        ('Canceled', 'Canceled'),
-    )
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    price = models.FloatField()
-    amount = models.FloatField()
-    status = models.CharField(max_length=10, choices=STATUS, default='New')
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.product.title
